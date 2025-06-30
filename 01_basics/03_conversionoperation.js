664db5a5c1d9c026d138930fdaa00b1c523359f4
nibodh07 <152626396+nibodh07@users.noreply.github.com>
2025-06-30 09:11:41 +0000

    learnt coversion

---
+++ 01_basics/03_conversionoperation.js
@@ -0,0 +1,34 @@
+let score = "Nibodh"
+
+console.log(typeof score)
+console.log(typeof(score))// two ways to identify the datatype.
+
+let valueinNumber= Number(score)
+console.log(typeof valueinNumber)
+console.log(valueinNumber)
+
+//number = number//
+//example 23=23//
+//789abc= string//
+//true =1 , false=0//
+
+let isloggedin= 0
+
+let booleanisloggedin= Boolean(isloggedin)
+console.log(booleanisloggedin);
+
+//0 k liye false //
+//1 k liye true//
+//empty string k liye false//
+//name k liye true//
+
+
+let somenumber = 23
+
+let stringNumber = String(somenumber)
+console.log(somenumber);
+console.log(typeof stringNumber);
+
+
+
+
