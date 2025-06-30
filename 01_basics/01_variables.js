e42e9ded9364ee7efcadf77c616fe7d7c01619bf
nibodh07 <152626396+nibodh07@users.noreply.github.com>
2025-06-30 08:27:52 +0000

    datatypes learnt

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,14 @@
+const accountId = 87090
+let accountEmail = "positive52@gmail.com"
+var accountPassword = "9089179"
+accountCity = "Nagpur"
+let accountState
+
+accountEmail="negative87@gmail.com"
+accountPassword="92829100"
+accountCity="mumbai"
+
+//accountId=2 constant can only be 1 not more than one//
+/* prefer not to use var because of blocking scope and functional scope*/
+console.log(accountId);
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])//
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,28 @@
+"use strict";// this is used because this executes the code in new format of JS!!//
+
+//alert(3+3); we are running alert on node js not on browser so it will give an error//
+
+console.log(98*65); // we can perform arithmetic operations on console log.//
+//code should be readable.//
+
+console.log("Nibodh");
+
+let Name = "Nibodh"
+let age = 21
+let isloggedin = false
+let state;
+//isloggedin means check karne k liye user logged in hai ki nahi//
+
+//number ki value is = 2 to the power of 53//
+//bigInt = large values than the number datatype//
+//string = textual data letters,word,sentences//
+//boolean = true false statement//
+//null = standalone value//
+//unefined = the thing which is not defined//
+//symbol =unique identifiers//
+
+console.log(typeof null) // null gives you you output as an object//
+console.log(typeof undefined) //undefined gives you output as undefined// 
+
+
+
