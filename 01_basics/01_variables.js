ee3f944bc1201930e9577d5602243ff20b6b1dab
nibodh07 <152626396+nibodh07@users.noreply.github.com>
2025-07-02 07:42:29 +0000

    conversions and comparison leart]nt

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -1,7 +1,7 @@
 const accountId = 87090
 let accountEmail = "positive52@gmail.com"
 var accountPassword = "9089179"
-accountCity = "Nagpur"
+accountId = "Nagpur"
 let accountState
 
 accountEmail="negative87@gmail.com"
@@ -11,4 +11,4 @@ accountCity="mumbai"
 //accountId=2 constant can only be 1 not more than one//
 /* prefer not to use var because of blocking scope and functional scope*/
 console.log(accountId);
-console.table([accountId,accountEmail,accountPassword,accountCity,accountState])//
+console.table([accountId,accountEmail,accountPassword, accountId ,accountState])//s
