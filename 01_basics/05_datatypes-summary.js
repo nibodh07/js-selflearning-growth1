c53152eeb89ebf0cec3b66e9aed4d2a0308e29fa
nibodh07 <152626396+nibodh07@users.noreply.github.com>
2025-07-02 11:34:41 +0000

    learnt dataypes summary

---
+++ 01_basics/05_datatypes-summary.js
@@ -0,0 +1,39 @@
+//primitives
+
+// 7 types - string , Number , Boolean , null , undefined , symbol, Bigint
+
+const score = 100
+const scorevalue = 100.23
+
+const isloggedin = false 
+const outsidetemp = null
+let useremail;
+
+
+const id = Symbol ('169')
+const anotherid = Symbol('169')
+
+console.log(id === anotherid);
+
+const bignumber = 2303091993n
+
+const heros = ["batman","superman","spiderman"];
+let myobj = {
+    name: "NicKY"
+    ,age: 22,
+}
+
+const myfunction = function(){
+    console.log("hello hope you are doing well");   
+}
+
+console.log(typeof myfunction);
+
+
+
+
+
+//reference type/non primitive
+// array
+//objects
+//functions
