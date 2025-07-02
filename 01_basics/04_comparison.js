e05d7fbe20e34a19dab6e1e95776d8360b0aaf08
nibodh07 <152626396+nibodh07@users.noreply.github.com>
2025-07-02 11:33:16 +0000

    comparison learnt

--- 01_basics/04_comparison.js
+++ 01_basics/04_comparison.js
@@ -1,1 +1,47 @@
-localStorage
+let str1 = "hello"
+let str2 = "  i am working on myself"
+
+
+let str3= str1 + str2
+//console.log(str3);
+
+//console.log("1"+2);
+//console.log(1+"2");
+//console.log("1"+"2");
+//console.log("1"+2+2);//if string first ismein use hogi then sabhi ko string mai mana jayega
+//console.log(2+2+"1");//if string last mai use hoti hai then only last wale ko string consider karege.
+
+//console.log(+true)
+//console.log(+"");
+
+
+
+let num1 , num2 , num3 
+num1=num2=num3=4+4
+//console.log(num3);
+
+let gamecounter= 89
+gamecounter++;            // if we + write this after the gamecounter it still adds 1.
+//console.log(gamecounter);
+
+let gamecounter1 = 89
+++gamecounter1;              // if we + write this before the gamecounter1 it still adds 1.
+//console.log(gamecounter1);
+
+//console.log(2<3);
+//console.log(2<=4);
+//console.log(2>1);
+//console.log(2>=5);
+//console.log(2!=8);
+
+console.log(null>0);
+console.log(null ==0 ); // in javascript there is different comparison and equality check and == has different syntax and === has different syntax.
+console.log(null>=0);
+
+console.log(undefined>0);
+console.log(undefined==0); 
+console.log(undefined<0);
+
+// === this is used for strict checking and as well as datatype too.
+
+console.log("2"=== 2);
