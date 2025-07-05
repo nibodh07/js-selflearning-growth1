87f73973fe6e1419a35dacb2e005c13fa0ac6089
nibodh07 <152626396+nibodh07@users.noreply.github.com>
2025-07-05 07:38:55 +0000

    nums and maths

---
+++ 01_basics/07_nums_and_maths.js
@@ -0,0 +1,49 @@
+const score = 400
+//console.log(score);
+
+const balance = new Number(100)
+//console.log(balance);
+
+//console.log(balance.toString().length);
+//console.log(balance.toFixed(1));
+
+
+const othernumber = 29810.98
+//console.log(othernumber.toPrecision(4));
+
+const hundreds = 1000000000
+//console.log(hundreds.toLocaleString('en-IN'));
+
+/////////////////////MATHS//////////////////////////////////////
+
+//console.log(Math);
+//console.log(Math.abs(-69));
+//console.log(Math.round(4,7));
+//console.log(Math.ceil(8,90));
+//console.log(Math.floor(89,88));
+
+console.log(Math.random());
+console.log(Math.floor(Math.random()*10 + 1));
+
+const min = 20
+const max = 10
+
+console.log(Math.floor(Math.random()  * (max - min + 1)) + min)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
