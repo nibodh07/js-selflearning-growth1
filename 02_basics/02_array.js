d47f061974560453b8138890d09a4e8ae7e01252
nibodh07 <152626396+nibodh07@users.noreply.github.com>
2025-07-08 12:26:39 +0000

    done learning arrays

---
+++ 02_basics/02_array.js
@@ -0,0 +1,31 @@
+const marvelheros = ["thor","ironman","superman"]
+const dc = ["superman","batman","flash"]
+
+marvelheros.push(dc)
+//console.log(marvelheros);
+//console.log(marvelheros[3][2]);
+
+const allmarvelheros = marvelheros.concat(dc)
+//console.log(allmarvelheros);
+
+const anotherarray = [1,2,3,[4,5,6],7,[6,7,[4,5]]]
+const realanotherarray = anotherarray.flat(Infinity)
+//console.log(realanotherarray);
+
+//console.log(Array.isArray("Nibodh"));
+//console.log(Array.from({name: "nibodh" })) // this is intresting part of the arrays
+
+let score1 = 100
+let score2 = 200
+let score3 = 300
+
+//console.log(Array.of(score1,score2,score3));
+
+
+
+
+
+
+
+
+
