71cce9d9adb3010830680d028cb1ee268fd3a5af
nibodh07 <152626396+nibodh07@users.noreply.github.com>
2025-07-08 09:05:34 +0000

    learning arrays

---
+++ 02_basics/01_arraysinjs.js
@@ -0,0 +1,45 @@
+// ARRAYS //
+
+const myArr = [0,1,2,3,4,5]
+const myheros = ["superman , batman , catman ,dogman"]
+
+const mynewarr = [0,1,3]
+
+//console.log(myArr)
+
+//console.log(mynewarr[1]);
+
+//console.log(myheros);
+
+//myArr.push(6)
+//myArr.push(7,8)
+//myArr.pop()
+
+//myArr.unshift(8)
+//myArr.shift()
+
+//console.log(myArr.includes(9));
+//console.log(myArr.indexOf(3));
+
+//const newarr = myArr.join()
+//onsole.log( typeof newarr);
+//console.log(myArr);
+
+// Slice , spLice //
+
+console.log("A",myArr);
+const myn1 = myArr.slice(1,3)
+
+console.log(myn1);
+console.log("B",myArr);
+
+const myn2 = myArr.splice(1,3)
+console.log("C",myArr);
+console.log(myn2);
+
+
+
+
+
+
+
